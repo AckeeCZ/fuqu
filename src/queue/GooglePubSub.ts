05972e6014095ad135bb07155ca72145256cad98
Štefan Prokop <sprokop1010@seznam.cz>
2019-09-24 08:42:41 +0200

    Add custom logger logging

--- src/queue/GooglePubSub.ts
+++ src/queue/GooglePubSub.ts
@@ -1,13 +1,15 @@
 
-import { Message as PubSubMessage, PubSub, Subscription, Topic, SubscriptionOptions } from '@google-cloud/pubsub';
-import { FuquBaseOptions, FuquOperations } from './Fuqu';
+import { Message as PubSubMessage, PubSub, Subscription, SubscriptionOptions, Topic } from '@google-cloud/pubsub';
 import { ClientConfig } from '@google-cloud/pubsub/build/src/pubsub';
+import Logger from '../Logger';
+import { FuquBaseOptions, FuquOperations } from './Fuqu';
 
 // https://cloud.google.com/nodejs/docs/reference/pubsub/0.23.x/Subscription
 const defaultMaxMessages = 100; // By default Subscription objects allow you to process 100 messages at the same time
 
 export interface GooglePubSubOptions extends FuquBaseOptions, ClientConfig {
     logger?: any;
+    debug?: boolean;
     topicName: string;
 }
 
@@ -19,32 +21,34 @@ export type OriginalMessage = PubSubMessage;
 
 export class GooglePubSub<D extends object> implements FuquOperations<D, PubSubMessage> {
     private googlePubSub: PubSub;
-    private logger: any;
+    private logger: Logger;
     private readonly topic: Promise<Topic>;
     private readonly subscription: Promise<Subscription>;
     private readonly topicName: string;
+    private readonly debug: boolean;
     constructor(options: GooglePubSubOptions) {
         this.googlePubSub = new PubSub(options);
-        this.logger = options.logger || console;
+        this.logger = new Logger(options.logger || console, !!options.debug);
+        this.debug = !!options.debug;
         this.topicName = options.topicName;
         this.topic = this.initTopic();
         this.subscription = this.initSubscription(GooglePubSub.getSubscriptionOptions(options));
     }
     public in: FuquOperations<D, PubSubMessage>['in'] = async data => {
-        this.logger.info(data, `Publishing message to the '${this.topicName}' topic`);
+        this.logger.logDebug(`Publishing message to the '${this.topicName}' topic`, data);
         try {
             typeof data === 'string'
                 ? (await this.topic)
                     .publish(Buffer.from(JSON.stringify(data)))
                 : (await this.topic)
                     .publishJSON(data);
-            this.logger.info(data, `Message successfully published to the '${this.topicName}' topic`);
+            this.logger.logDebug(`Message successfully published to the '${this.topicName}' topic`, data);
         } catch (e) {
-            this.logger.error(e, `Message publishing to the '${this.topicName}' topic failed: ${e.message}`);
+            this.logger.logError(`Message publishing to the '${this.topicName}' topic failed: ${e.message}`, e);
         }
     }
     public off: FuquOperations<D, PubSubMessage>['off'] = async cb => {
-        this.logger.info(`Trying to register subscription '${this.topicName}' callback`);
+        this.logger.logDebug(`Trying to register subscription '${this.topicName}' callback`);
         (await this.subscription).on(`message`, (message: PubSubMessage) => {
             cb({
                 id: message.id,
@@ -54,15 +58,15 @@ export class GooglePubSub<D extends object> implements FuquOperations<D, PubSubM
                 original: message,
             });
         });
-        this.logger.info(`Listening on ${this.topicName} ... ready for fuq ...`);
+        this.logger.logDebug(`Listening on ${this.topicName} ... ready for fuq ...`);
     }
     private async initTopic() {
-        this.logger.info(`Initializing the '${this.topicName}' topic`);
+        this.logger.logDebug(`Initializing the '${this.topicName}' topic`);
         const [topic] = await (await this.googlePubSub).topic(this.topicName).get({ autoCreate: true });
         return topic;
     }
     private async initSubscription(subscriptionOptions?: SubscriptionOptions) {
-        this.logger.info(`Initializing the '${this.topicName}' subscription`);
+        this.logger.logDebug(`Initializing the '${this.topicName}' subscription`);
         const [subscription] = await (await this.topic).subscription(this.topicName, subscriptionOptions).get({ autoCreate: true });
         return subscription;
     }
@@ -73,7 +77,7 @@ export class GooglePubSub<D extends object> implements FuquOperations<D, PubSubM
                     allowExcessMessages: false,
                     maxMessages: queueOptions.queue.maxMessages || defaultMaxMessages,
                 },
-            }
+            };
         }
     }
 }
