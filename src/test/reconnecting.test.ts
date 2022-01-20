7bffa91dc7882d9d0f0515aaed447f1c609a5ac1
Jaroslav Šmolík <grissius@gmail.com>
2022-01-20 08:34:11 +0100

    ✅️ Update test memoery mock

--- src/test/reconnecting.test.ts
+++ src/test/reconnecting.test.ts
@@ -1,50 +1,13 @@
 import { FuQu } from '../index'
 
 import test from 'ava'
+import { SubscriptionOptionsLike } from '../lib/contracts/pubsub'
 
-const pubsubSerialMockFactory = () => {
-  let handlers: any[] = []
-  let messages: any[] = []
-  let subscribersHooked = 0
-  let consumerMutex = false
-  const consume: any = async () => {
-    if (messages.length === 0 || consumerMutex) return
-    consumerMutex = true
-    const m = messages.pop()
-    for (const h of handlers) {
-      await h(m)
-    }
-    consumerMutex = false
-    return consume()
-  }
-  return {
-    getHooked: () => subscribersHooked,
-    factory: () => {
-      return {
-        topic: (_: string) => ({
-          publishMessage: async (options: any) => {
-            messages.push(options)
-            consume()
-            return Date.now()
-          }
-        }),
-        subscription: (_: string) => ({
-          on: (_: string, listener: (...args: any[]) => void) => {
-            subscribersHooked++
-            handlers.push(listener)
-          },
-          removeAllListeners: () => {
-            handlers = []
-          },
-        }),
-      }
-    }
-  }
-}
+
 const RECONNECT_TIMEOUT_MILLIS = 50
 test('Does not reconnect while handling messages', async t => {
-  const { factory, getHooked } = pubsubSerialMockFactory()
-  const fuQu = FuQu<any, any, any>(factory, null as any, { reconnectAfterMillis: RECONNECT_TIMEOUT_MILLIS })
+  const pubSubMock = pubsubSerialMockFactory()
+  const fuQu = FuQu(pubSubMock.factory, null as any, { reconnectAfterMillis: RECONNECT_TIMEOUT_MILLIS, batching: { maxMessages: 1 } })
 
   const MESSAGE_COUNT = 3
   fuQu.createSubscriber('', async () => {
@@ -55,12 +18,12 @@ test('Does not reconnect while handling messages', async t => {
     await p.publish({})
   }
   await new Promise(resolve => setTimeout(resolve, RECONNECT_TIMEOUT_MILLIS * MESSAGE_COUNT))
-  t.is(getHooked(), 1) // initial
+  t.is(pubSubMock.timesHooked, 1) // initial
 })
 
 test('Does keep reconnecting when dry', async t => {
-  const { factory, getHooked } = pubsubSerialMockFactory()
-  const fuQu = FuQu<any, any, any>(factory, null as any, { reconnectAfterMillis: RECONNECT_TIMEOUT_MILLIS })
+  const pubSubMock = pubsubSerialMockFactory()
+  const fuQu = FuQu(pubSubMock.factory, null as any, { reconnectAfterMillis: RECONNECT_TIMEOUT_MILLIS, batching: { maxMessages: 1 } })
   const MESSAGE_COUNT = 3
   fuQu.createSubscriber('', () => {})
   const p = fuQu.createPublisher('')
@@ -68,5 +31,51 @@ test('Does keep reconnecting when dry', async t => {
     await p.publish({})
     await new Promise(resolve => setTimeout(resolve, RECONNECT_TIMEOUT_MILLIS))
   }
-  t.is(getHooked(), MESSAGE_COUNT + 1) // initial + after each message
+  t.is(pubSubMock.timesHooked, MESSAGE_COUNT + 1) // initial + after each message
 })
+
+const pubsubSerialMockFactory = () => {
+  let handlers: any[] = []
+  let messages: any[] = []
+  let subscribersHooked = 0
+  let consumerSemaphore = 0
+  let maxConcurrentMessages = 1
+  const consume: any = async () => {
+    if (messages.length === 0 || consumerSemaphore === maxConcurrentMessages) return
+    consumerSemaphore++
+    const m = messages.pop()
+    for (const h of handlers) {
+      await h(m)
+    }
+    consumerSemaphore--
+    return consume()
+  }
+  return {
+    get timesHooked () {
+      return subscribersHooked
+    },
+    factory: () => {
+      return {
+        topic: (_: string) => ({
+          publishMessage: async (options: any) => {
+            messages.push(options)
+            consume()
+            return Date.now()
+          }
+        }),
+        subscription: (_: string, options?: SubscriptionOptionsLike) => {
+          maxConcurrentMessages = options?.batching?.maxMessages ?? 1
+          return {
+            on: (_: string, listener: (...args: any[]) => void) => {
+              subscribersHooked++
+              handlers.push(listener)
+            },
+            removeAllListeners: () => {
+              handlers = []
+            },
+          }
+        },
+      }
+    }
+  }
+}
