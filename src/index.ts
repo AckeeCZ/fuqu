45baff0a68a535d98e88175ff1e75d5ad90a4b4d
Jaroslav Šmolík <grissius@gmail.com>
2022-03-22 13:52:35 +0100

    ✨️ Add exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+export { FuQuOptions } from './lib/core/fuqu-factory'
+export { FuQuSubscriberOptions, MessageHandler } from './lib/core/components/subscriber'
+export { FuQuPublisher } from './lib/core/components/publisher'
+export { SubscriptionOptionsLike } from "./lib/contracts/pubsub";
 export { FuQu } from './lib/core/fuqu-factory'
