428b33ba58275ae78fe9e8e17062e46c36fcebd1
Jaroslav Šmolík <grissius@gmail.com>
2022-03-24 10:11:51 +0100

    ✨️ Export Logger interface

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { FuQuOptions } from './lib/core/fuqu-factory'
 export { FuQuSubscriberOptions, MessageHandler } from './lib/core/components/subscriber'
 export { FuQuPublisher } from './lib/core/components/publisher'
 export { SubscriptionOptionsLike } from "./lib/contracts/pubsub";
+export { Logger } from "./lib/contracts/logger";
 export { FuQu } from './lib/core/fuqu-factory'
